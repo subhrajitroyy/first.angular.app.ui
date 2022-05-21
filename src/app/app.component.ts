b700e4cc3a0fcef1308666605f11a9896e0d61f3
Subhrajit Roy <subhrajit.royy@gmail.com>
2022-05-21 20:35:28 +1000

    important code snippets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  // h3 {
+  //   color: dodgerblue;
+  // }`]
 })
 export class AppComponent {
-  title = 'MyFirstAngularApp';
+  showSecret = false;
+  log = [];
+  //username = '';
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
